68646d936d44f02da504273199cd549adb747bf6
Etem SARAC <esay35@gmail.com>
2024-04-04 14:56:48 +0300

    BKI if statements simplified!

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -3,8 +3,8 @@ import math
 
 window = Tk()
 window.title('BMI_Calculator')
-#window.iconbitmap('logo_1.ico')
-window.minsize(600, 600)
+window.iconbitmap('logo_1.ico')
+window.minsize(600, 300)
 window.config(padx=20, pady=20)
 
 label = Label(window, text='Enter your Weight(kg)')
@@ -12,7 +12,6 @@ label.pack()
 entry_weight = Entry()
 entry_weight.focus()
 entry_weight.pack()
-
 
 label1 = Label(window, text='Enter your Height(cm)')
 label1.pack()
@@ -29,23 +28,26 @@ def button_clicked():
         answer.config(text='Please write a number here!')
 
     BKI = (a/((b*b)*0.01**2))
-    if BKI < 18.5:
+    if BKI <= 18.5:
         label3 = Label(window,text='According to Your Data: \nHeight = {}, Weight = {} ve BKI = {}'.format(b,a, BKI))
         answer.config(text='Thin')
         label3.pack()
 
-    elif BKI > 18.5 and BKI < 25:
+    elif 18.5 < BKI <= 25:
         label4 = Label(window,text='According to Your Data: \nHeight = {}, Weight = {} ve BKI = {}'.format(b,a, BKI))
         answer.config(text='Normal')
         label4.pack()
-    elif BKI > 25 and BKI < 30:
+
+    elif 25 < BKI <= 30:
         label5 = Label(window,text='According to Your Data: \nHeight = {}, Weight = {} ve BKI = {}'.format(b,a, BKI))
         answer.config(text='Over Weight!')
         label5.pack()
+
     elif BKI > 30:
         label6 = Label(window,text='According to Your Data: \nHeight = {}, Weight = {} ve BKI = {}'.format(b,a, BKI))
         answer.config(text='Go to Doctor!!!')
         label6.pack()
+
 button1 = Button(text='Calculate', command=button_clicked)
 button1.pack()
 
